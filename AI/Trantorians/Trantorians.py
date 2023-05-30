b50226f3f197f4c877de35f1119c018fba0c06fc
Pierre-Louis LEROY <pierre-louis.leroy@epitech.eu>
2023-05-30 13:56:06 +0200

    feat: add first class for tratanrians for AI

---
+++ AI/Trantorians/Trantorians.py
@@ -0,0 +1,12 @@
+##
+## EPITECH PROJECT, 2022
+## B-YEP-400-PAR-4-1-zappy-viktor.bruggeman
+## File description:
+## Trantorians.py
+##
+
+class Trantorians:
+    food: int = 126
+    found = []
+    fov: int = 1
+    sound = 0
