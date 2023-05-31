533af658eaefd2f2827186d55b1984509ef6b615
BENAIS--CAPTAL <damien.benais-captal@epitech.eu>
2023-05-31 15:11:20 +0200

    feat: send team name and receive confirmation from server

--- AI/Client/Client.py
+++ AI/Client/Client.py
@@ -11,12 +11,16 @@ def connect_to_server(port, name, machine):
     server_ip = machine
     server_port = port
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
     client_socket.connect((server_ip, server_port))
-
     print('Connected to the server.')
-    data = 'Hello, server!'
-    client_socket.sendall(data.encode())
 
+    received_data = client_socket.recv(1024).decode()
+    print('Received from server:', received_data)
+
+    test = name + '\n'
+    client_socket.send(test.encode())
+    print('Sent to server:', test)
 
     received_data = client_socket.recv(1024).decode()
     print('Received from server:', received_data)
