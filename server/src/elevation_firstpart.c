bed799cc29bd3b59d0ce0ee41b465dda394502a9
Bastien <bastien.rodrigues@epitech.eu>
2023-06-09 10:38:21 +0200

    feat: modif parameter for elevation function

--- server/src/elevation_firstpart.c
+++ server/src/elevation_firstpart.c
@@ -9,32 +9,32 @@
 
 void elevation_lvl2(team_t *team)
 {
-    if (team->players->linemate == 1 && team->players->deraumere == 1
-    && team->players->sibur == 1 && team->players->mendiane == 0 
-    && team->players->phiras == 0 && team->players->thystame == 0)
+    if (team->players->linemate >= 1 && team->players->deraumere >= 1
+    && team->players->sibur >= 1 && team->players->mendiane >= 0 
+    && team->players->phiras >= 0 && team->players->thystame >= 0)
         team->players->lvl = 2;
 }
 
 void elevation_lvl3(team_t *team)
 {
-    if (team->players->linemate == 2 && team->players->deraumere == 0
-    && team->players->sibur == 1 && team->players->mendiane == 0 
-    && team->players->phiras == 2 && team->players->thystame == 0)
+    if (team->players->linemate >= 2 && team->players->deraumere >= 0
+    && team->players->sibur >= 1 && team->players->mendiane >= 0 
+    && team->players->phiras >= 2 && team->players->thystame >= 0)
         team->players->lvl = 3;
 }
 
 void elevation_lvl4(team_t *team)
 {
-    if (team->players->linemate == 1 && team->players->deraumere == 1
-    && team->players->sibur == 2 && team->players->mendiane == 0 
-    && team->players->phiras == 1 && team->players->thystame == 0)
+    if (team->players->linemate >= 1 && team->players->deraumere >= 1
+    && team->players->sibur >= 2 && team->players->mendiane >= 0 
+    && team->players->phiras >= 1 && team->players->thystame >= 0)
         team->players->lvl = 4;
 }
 
 void elevation_lvl5(team_t *team)
 {
-    if (team->players->linemate == 1 && team->players->deraumere == 2
-    && team->players->sibur == 1 && team->players->mendiane == 3 
-    && team->players->phiras == 0 && team->players->thystame == 0)
+    if (team->players->linemate >= 1 && team->players->deraumere >= 2
+    && team->players->sibur >= 1 && team->players->mendiane >= 3 
+    && team->players->phiras >= 0 && team->players->thystame >= 0)
         team->players->lvl = 5;
 }
