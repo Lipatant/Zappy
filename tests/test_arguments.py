fe74cd21065e10ab23ed517b23de4b96362fb5b8
Pierre-Louis LEROY <pierre-louis.leroy@epitech.eu>
2023-06-09 10:57:25 +0200

    test: adding test_arguments, first unit_test of ai

--- tests/test_arguments.py
+++ tests/test_arguments.py
@@ -5,45 +5,30 @@
 ## tests/test_arguments.py
 ##
 
-'''
-port: int
-team: str
-ip: str
-
-def __init__(self) -> None:
-    self.port = 0
-    self.team = ""
-    self.ip = "127.0.0.1"
-
-self = 0
-__init__(self)
-'''
-#import sys
-#sys.path.append("../")
 import pytest
+from ai.arguments.arguments import arguments
 
-#from ai.arguments.arguments import arguments
-from arguments import Arguments
 '''
 def test_init():
-    obj = Arguments
+    obj = arguments
     assert obj.port == 0
     assert obj.team == ""
     assert obj.ip == "127.0.0.1"
 '''
+
 def test_parse_args_valid():
-    obj = Arguments
+    obj = arguments
     obj.parse_args(0, 6, ["-p", "8080", "-n", "MyTeam", "-h", "192.168.0.1"])
     assert obj.port == 8080
     assert obj.team == "MyTeam"
     assert obj.ip == "192.168.0.1"
 
 def test_parse_args_invalid():
-    obj = Arguments
+    obj = arguments
     with pytest.raises(SystemExit):
         obj.parse_args(0, 4, ["-p", "8080", "-x", "InvalidArg"])
 
 def test_parse_args_incomplete():
-    obj = Arguments
+    obj = arguments
     with pytest.raises(SystemExit):
         obj.parse_args(0, 4, ["-p", "8080", "-n", "MyTeam"])
