4c9011076e3fb643e2ad3beb3abd2d960eb2db70
Bastien <bastien.rodrigues@epitech.eu>
2023-06-09 15:53:26 +0200

    feat: modif elevation function by adding deletion of some ressource after elevation

--- server/src/elevation_secondpart.c
+++ server/src/elevation_secondpart.c
@@ -10,23 +10,38 @@
 void elevation_lvl6(team_t *team)
 {
     if (team->players->linemate >= 1 && team->players->deraumere >= 2
-    && team->players->sibur >= 3 && team->players->mendiane >= 0 && team->players->phiras >= 1
-    && team->players->thystame >= 0)
+    && team->players->sibur >= 3 && team->players->phiras >= 1) {
+        team->players->linemate -= 1;
+        team->players->deraumere -= 2;
+        team->players->sibur -= 3;
+        team->players->phiras -= 1;
         team->players->lvl = 6;
+    }
 }
 
 void elevation_lvl7(team_t *team)
 {
     if (team->players->linemate >= 2 && team->players->deraumere >= 2
-    && team->players->sibur >= 2 && team->players->mendiane >= 2 && team->players->phiras >= 2
-    && team->players->thystame >= 1)
+    && team->players->sibur >= 2 && team->players->phiras >= 2) {
+        team->players->linemate -= 1;
+        team->players->deraumere -= 2;
+        team->players->sibur -= 3;
+        team->players->phiras -= 1;
         team->players->lvl = 7;
+    }
 }
 
 void elevation_lvl8(team_t *team)
 {
-    if (team->players->linemate >= 0 && team->players->deraumere >= 0
-    && team->players->sibur >= 0 && team->players->mendiane >= 0 && team->players->phiras >= 0
-    && team->players->thystame >= 0)
+    if (team->players->linemate >= 2 && team->players->deraumere >= 2
+    && team->players->sibur >= 2 && team->players->mendiane >= 2 
+    && team->players->phiras >= 2 && team->players->thystame >= 1) {
+        team->players->linemate -= 2;
+        team->players->deraumere -= 2;
+        team->players->sibur -= 2;
+        team->players->mendiane -= 2;
+        team->players->phiras -= 2;
+        team->players->thystame -= 1;
         team->players->lvl = 8;
+    }
 }
