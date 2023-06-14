b2519dba47309a1d3a89ec5031b5ff5971131f6a
cyprien <cyprien.nguyen-van-vien@epitech.eu>
2023-06-14 11:17:38 +0200

    feat: command look for the server

---
+++ server/src/cmd/look.c
@@ -0,0 +1,109 @@
+/*
+** EPITECH PROJECT, 2023
+** Zappy
+** File description:
+** look
+*/
+
+#include "my.h"
+#include "server.h"
+#include "cmd.h"
+
+
+void look(data_t *data)
+{
+    if (direction == 0) {
+    } else if (direction == 1) {
+    } else if (direction == 2) {
+    } else if (direction == 3) {
+        look_left()
+    }
+    send(socket, to_send, strlen(to_send), 0);
+}
+
+void look_left(data_t *data)
+{
+    int j = 0;
+    int rank = 0;
+
+    for (int i = 0; i <= level; i++) {
+        j = i;
+        rank = y - i;
+        while (rank < 0)
+            rank += client_y - 1;
+        while (j > 0) {
+            printf("%c", tmp_map[x + j][rank]);
+            j--;
+        }
+        printf("%c", tmp_map[x][rank]);
+        for (int k = 1; k <= i; k++) {
+            printf("%c", tmp_map[x - k][rank]);
+        }
+        printf("\n");
+    }
+}
+
+void look_down(data_t *data)
+{
+    int j = 0;
+    int rank = 0;
+
+    for (int i = 0; i <= level; i++) {
+        j = i;
+        rank = x + i;
+        while (rank >= client_x)
+            rank -= client_x;
+        while (rank < 0)
+            rank += client_x;
+        for (int k = 1; k <= i; k++)
+            printf("%c", tmp_map[rank][y + k]);
+        printf("%c", tmp_map[rank][y]);
+        while (j > 0) {
+            printf("%c", tmp_map[rank][y - j]);
+            j--;
+        }
+        printf("\n");
+    }
+}
+
+void look_up(data_t *data)
+{
+    int j = 0;
+    int rank = 0;
+
+    for (int i = 0; i <= level; i++) {
+        j = i;
+        rank = x - i;
+        while (rank < 0)
+            rank += client_x;
+        while (j > 0) {
+            printf("%c", tmp_map[rank][y - j]);
+            j--;
+        }
+        printf("%c", tmp_map[rank][y]);
+        for (int k = 1; k <= i; k++)
+            printf("%c", tmp_map[rank][y + k]);
+        printf("\n");
+    }
+}
+
+void look_right(data_t *data)
+{
+    int j = 0;
+    int rank = 0;
+
+    for (int i = 0; i <= level; i++) {
+        j = i;
+        rank = y + i;
+        while (rank >= client_y)
+            rank -= client_y;
+        while (j > 0) {
+            printf("%c", tmp_map[x - j][rank]);
+            j--;
+        }
+        printf("%c", tmp_map[x][rank]);
+        for (int k = 1; k <= i; k++)
+            printf("%c", tmp_map[x + k][rank]);
+        printf("\n");
+    }
+}
