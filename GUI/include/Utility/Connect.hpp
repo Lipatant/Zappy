30e1d1cb884099f87e3a80a70183f217cfb1856b
cyprien <cyprien.nguyen-van-vien@epitech.eu>
2023-06-21 13:44:29 +0200

    feat: adding the documentation to Connect.hpp

--- GUI/include/Utility/Connect.hpp
+++ GUI/include/Utility/Connect.hpp
@@ -5,6 +5,9 @@
 ** Connect
 */
 
+/// @file
+/// @brief ...
+
 #ifndef CONNECT_HPP_
     #define CONNECT_HPP_
 
@@ -14,12 +17,29 @@
 
 class Connect {
     public:
+
+        /// @brief Constructor of the class Connect
+        /// @param string std::string ip
+        /// @param int int for the recuperation of the port4
         Connect(std::string ip, int port);
+
+        /// @brief Destructor of the class Connect
         ~Connect();
+
+        /// @brief Getter of the IP
         std::string getIp() const { return _ip; };
+
+        /// @brief Getter of the Port
         int getPort() const { return _port; };
-        std::string receive();
+
+        /// @brief Receive the message from the server
+        std::string receive(void);
+
+        /// @brief Send the GUI authentification to the server
+        /// @param string std::string for sending GUI to the server
         void sender(std::string);
+
+        /// @brief Connection to the server
         void join(void);
 
     protected:
@@ -31,8 +51,15 @@ class Connect {
         struct sockaddr_in _serverAddress;
         class Exception : public std::exception {
             public:
+
+                ///@brief Exeception handles error feedback
+                ///@param string std::string message
                 Exception(std::string message) : _message(message) {};
+
+                ///@brief DestructorExeception handles error feedback
+                ///@param string std::string message
                 ~Exception() = default;
+
                 const char *what() const noexcept override { return _message.c_str(); };
             private:
                 std::string _message;
