4a6bca7941befae35c299a292110c493595294c2
Bastien <bastien.rodrigues@epitech.eu>
2023-06-21 16:06:00 +0200

    feat: delete srand inside main function

--- server/src/main.c
+++ server/src/main.c
@@ -141,7 +141,6 @@ int main(int argc, char **argv)
 
     if (argc == 2 && strcmp(argv[1], "-help") == 0)
         return help();
-    srand(time(NULL));
     args.names = malloc(sizeof(char *));
     args.names[0] = NULL;
     args = check_args(argc, argv, args);
