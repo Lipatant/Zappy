99a02b3e1aa54c5284b481e0857e9cc8106031e3
Drindael <cyprien.nguyen-van-vien@epitech.eu>
2023-06-24 14:36:47 +0200

    fix: coding style our_time.h in the server

--- server/include/our_time.h
+++ server/include/our_time.h
@@ -9,7 +9,7 @@
 
 #ifndef OUR_TIME_H_
     #define OUR_TIME_H_
-    
+
     //value to convert our seconds into microseconds
     #define CONVERT_SEC 1000000
-#ifndef /* !OUR_TIME_H_ */
+#endif /* !OUR_TIME_H_ */
